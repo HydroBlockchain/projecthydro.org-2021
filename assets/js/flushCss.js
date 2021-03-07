87aeac2eccf5e693d554750095dd5bbf2f70e6dd
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2021-03-07 01:17:43 +0100

    cleanup

--- assets/js/flushCss.js
+++ assets/js/flushCss.js
@@ -2,7 +2,6 @@ function reloadStylesheets() {
     var queryString = '?reload=' + new Date().getTime();
     $('link[rel="stylesheet"]').each(function () {
         this.href = this.href.replace(/\?.*|$/, queryString);
-        console.log("CSS Reloaded!")
     });
 }
 
